139774e9c715382b1cd700e0beabaca52399960a
Humberto Yances <hyances@gmail.com>
2016-01-02 09:57:36 -0500

    agregado simpleforms para envio de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,8 +18,8 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'hyances@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contacto OSM Colombia:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "Usted ha recibido un nuevo mensaje desde el formulario de contactos.\n\n"."Aqu&iacute; están los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
+$headers = "From: info@openstreetmap.co\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
